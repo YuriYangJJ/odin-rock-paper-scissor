8e7c357afdd35a6d78c8609f4fce87876ab7284b
YuriYangJJ <138073242+YuriYangJJ@users.noreply.github.com>
2024-03-01 15:50:30 +0800

    Create playRound which play rock paper scissor

--- myscripts.js
+++ myscripts.js
@@ -6,3 +6,20 @@ function getRandomInt(max) {
 function getComputerChoice() {
     return ['Rock', 'Paper', 'Scissors'][getRandomInt(3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+    if (playerSelection == computerSelection) {
+        return "tie"
+    }
+    else if(playerSelection == 'rock' && computerSelection == 'paper' ||
+            playerSelection == 'paper' && computerSelection == 'scissors' ||
+            playerSelection == 'scissors' && computerSelection == 'rock') {
+        // player lose
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+}
